b533647ee27d5d73616f89d86a24c20d3586726d
Jaecen <jason@addington.com>
2013-06-13 00:04:40 -0700

    Fix ajax URL's.

--- src/Taskit.Web/Content/Cards-Index.js
+++ src/Taskit.Web/Content/Cards-Index.js
@@ -1,4 +1,4 @@
-﻿$(function () {
+﻿$(document).ready(function () {
 	//firefox saves the last selection so you need to manaully set he select box.
 	$('#sortingAttribute').val('@Model.SortingAttribute');
 
@@ -10,19 +10,18 @@
 			var attributeKey = $(this).data("attribute-key");
 			var attributeValue = $(this).data("attribute-value");
 
-			$.post('@Url.Action("UpdateCardAttribute", "Cards")', {
+			$.post(window.links.updateCardAttribute, {
 				'cardId': cardId,
 				'attributeKey': attributeKey,
 				'attributeValue': attributeValue
 			})
 		},
 		update: function (event, ui) {
-			//console.log(this, event, ui);
 			var cardId = ui.item.data('card-id');
 			var previousCardId = ui.item.prev('.card').first().data('card-id');
 			var attributeKey = $(this).data("attribute-key");
 
-			$.post('@Url.Action("UpdateCardDisplayOrder", "Cards")', {
+			$.post(window.links.updateCardDisplayOrder, {
 				'cardId': cardId,
 				'previousCardId': previousCardId,
 				'attributeKey': attributeKey
