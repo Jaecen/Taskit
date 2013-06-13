35d59b18ba9444aa3ce47bb8e28be05ed6d05fa9
Jaecen <jason@addington.com>
2013-06-12 23:39:28 -0700

    Move view JS to a separate file. Fix view whitespace.

---
+++ src/Taskit.Web/Content/Cards-Index.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+	//firefox saves the last selection so you need to manaully set he select box.
+	$('#sortingAttribute').val('@Model.SortingAttribute');
+
+	$('.cardList').sortable({
+		revert: true,
+		connectWith: '.cardList',
+		receive: function (event, ui) {
+			var cardId = ui.item.data('card-id');
+			var attributeKey = $(this).data("attribute-key");
+			var attributeValue = $(this).data("attribute-value");
+
+			$.post('@Url.Action("UpdateCardAttribute", "Cards")', {
+				'cardId': cardId,
+				'attributeKey': attributeKey,
+				'attributeValue': attributeValue
+			})
+		},
+		update: function (event, ui) {
+			//console.log(this, event, ui);
+			var cardId = ui.item.data('card-id');
+			var previousCardId = ui.item.prev('.card').first().data('card-id');
+			var attributeKey = $(this).data("attribute-key");
+
+			$.post('@Url.Action("UpdateCardDisplayOrder", "Cards")', {
+				'cardId': cardId,
+				'previousCardId': previousCardId,
+				'attributeKey': attributeKey
+			})
+		}
+	});
+
+});
